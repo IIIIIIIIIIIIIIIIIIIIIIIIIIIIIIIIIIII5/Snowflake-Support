9fe3840d40ddcfdc3193112852ad1c82e845f649
IIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII5 <jetpacked3@gmail.com>
2025-10-11 10:24:08 -0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,41 @@
+import { Client, Collection, GatewayIntentBits } from 'discord.js';
+import fs from 'fs';
+
+const TOKEN = process.env.TOKEN;
+
+const client = new Client({
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMembers,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.MessageContent
+    ]
+});
+
+client.commands = new Collection();
+
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+for (const file of commandFiles) {
+    const command = await import(`./commands/${file}`);
+    client.commands.set(command.default.data.name, command.default);
+}
+
+client.once('ready', () => {
+    console.log(`${client.user.tag} is online!`);
+});
+
+client.on('interactionCreate', async interaction => {
+    if (!interaction.isChatInputCommand()) return;
+
+    const command = client.commands.get(interaction.commandName);
+    if (!command) return;
+
+    try {
+        await command.execute(interaction);
+    } catch (error) {
+        console.error(error);
+        await interaction.reply({ content: 'There was an error executing this command.', ephemeral: true });
+    }
+});
+
+client.login(TOKEN);
