be6f4af39f462aff6321b9292d7ced1ed685da3f
IIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII5 <jetpacked3@gmail.com>
2025-10-12 00:00:38 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import { Client, Collection, GatewayIntentBits, Partials } from "discord.js";
+import { Client, Collection, GatewayIntentBits, Partials, REST, Routes } from "discord.js";
 import fs from "fs";
 import path from "path";
 
@@ -9,10 +9,12 @@ const client = new Client({
 
 client.commands = new Collection();
 
+const commands = [];
 const commandsPath = path.join(process.cwd(), "commands");
 for (const file of fs.readdirSync(commandsPath).filter(f => f.endsWith(".js"))) {
   const command = await import(`./commands/${file}`);
   client.commands.set(command.default.data.name, command.default);
+  commands.push(command.default.data.toJSON());
 }
 
 const eventsPath = path.join(process.cwd(), "events");
@@ -24,5 +26,14 @@ for (const file of fs.readdirSync(eventsPath).filter(f => f.endsWith(".js"))) {
     client.on(event.default.name, (...args) => event.default.execute(...args, client));
   }
 }
+
+const rest = new REST({ version: "10" }).setToken(process.env.TOKEN);
+
+(async () => {
+  await rest.put(
+    Routes.applicationCommands(process.env.CLIENT_ID),
+    { body: commands }
+  );
+})();
 
 client.login(process.env.TOKEN);
