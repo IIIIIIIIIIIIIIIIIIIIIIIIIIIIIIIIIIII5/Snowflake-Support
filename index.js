01038c7f52a6ffe57ad5ac99a03e1d9e81dea894
IIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII5 <jetpacked3@gmail.com>
2025-11-26 12:08:42 -0500

    Update index.js

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ const rest = new REST({ version: "10" }).setToken(process.env.TOKEN);
 
 (async () => {
   await rest.put(
-    Routes.applicationCommands(process.env.CLIENT_ID, process.env.GUILD_ID),
+    Routes.applicationCommands(process.env.CLIENT_ID),
     { body: commands }
   );
 })();
