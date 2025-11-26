c334b6c36e34d13c383b281070f63259fae6d48f
IIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII5 <jetpacked3@gmail.com>
2025-11-26 11:46:26 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const { Client, Collection, GatewayIntentBits, Partials, REST, Routes, ActivityType } = require("discord.js");
-const fs = require("fs");
-const path = require("path");
+import { Client, Collection, GatewayIntentBits, Partials, REST, Routes, ActivityType  } from "discord.js";
+import fs from "fs";
+import path from "path";
 
 const client = new Client({
   intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages],
@@ -10,36 +10,30 @@ const client = new Client({
 client.commands = new Collection();
 
 const commands = [];
-
 const commandsPath = path.join(process.cwd(), "commands");
 for (const file of fs.readdirSync(commandsPath).filter(f => f.endsWith(".js"))) {
-  const command = require(`./commands/${file}`);
-  client.commands.set(command.data.name, command);
-  commands.push(command.data.toJSON());
+  const command = await import(`./commands/${file}`);
+  client.commands.set(command.default.data.name, command.default);
+  commands.push(command.default.data.toJSON());
 }
 
 const eventsPath = path.join(process.cwd(), "events");
 for (const file of fs.readdirSync(eventsPath).filter(f => f.endsWith(".js"))) {
-  const event = require(`./events/${file}`);
-  if (event.once) {
-    client.once(event.name, (...args) => event.execute(...args, client));
+  const event = await import(`./events/${file}`);
+  if (event.default.once) {
+    client.once(event.default.name, (...args) => event.default.execute(...args, client));
   } else {
-    client.on(event.name, (...args) => event.execute(...args, client));
+    client.on(event.default.name, (...args) => event.default.execute(...args, client));
   }
 }
 
 const rest = new REST({ version: "10" }).setToken(process.env.TOKEN);
 
 (async () => {
-  try {
-    await rest.put(
-      Routes.applicationCommands(process.env.CLIENT_ID),
-      { body: commands }
-    );
-    console.log("Slash commands registered.");
-  } catch (err) {
-    console.error("Failed to register commands:", err);
-  }
+  await rest.put(
+    Routes.applicationCommands(process.env.CLIENT_ID),
+    { body: commands }
+  );
 })();
 
 client.login(process.env.TOKEN);
